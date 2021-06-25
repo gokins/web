e830c60394115f51de3b950b69039d9ae1527f0f
jim <303600370@qq.com>
2021-06-25 21:34:28 +0800

    login

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -9,40 +9,41 @@
                 <CForm>
                   <h1>Login</h1>
                   <p class="text-muted">Sign In to your account</p>
-                  <CInput
-                    placeholder="Username"
-                    autocomplete="username email"
-                  >
-                    <template #prepend-content><CIcon name="cil-user"/></template>
+                  <CInput placeholder="Username" autocomplete="username email">
+                    <template #prepend-content
+                      ><CIcon name="cil-user"
+                    /></template>
                   </CInput>
                   <CInput
                     placeholder="Password"
                     type="password"
                     autocomplete="curent-password"
                   >
-                    <template #prepend-content><CIcon name="cil-lock-locked"/></template>
+                    <template #prepend-content
+                      ><CIcon name="cil-lock-locked"
+                    /></template>
                   </CInput>
                   <CRow>
                     <CCol col="6" class="text-left">
-                      <CButton color="primary" class="px-4">Login</CButton>
+                      <CButton color="info" class="px-4" @click="login">Login</CButton>
                     </CCol>
                     <CCol col="6" class="text-right">
-                      <CButton color="link" class="px-0">Forgot password?</CButton>
-                      <CButton color="link" class="d-lg-none">Register now!</CButton>
+                      <CButton color="link" class="px-0"
+                        >Forgot password?</CButton
+                      >
                     </CCol>
                   </CRow>
                 </CForm>
               </CCardBody>
             </CCard>
             <CCard
-              color="primary"
-              text-color="white"
+              text-color="info"
               class="text-center py-5 d-md-down-none"
               body-wrapper
             >
               <CCardBody>
-                <h2>Gokins: More Power</h2>
-                <img src="">
+                <img class="login-dog" src="/img/login/dog.png" />
+                <h4>Gokins: More Power</h4>
               </CCardBody>
             </CCard>
           </CCardGroup>
@@ -53,7 +54,30 @@
 </template>
 
 <script>
+import { Login } from "@/assets/js/apis";
 export default {
-  name: 'Login'
-}
+  name: "Login",
+  methods: {
+    login() {
+      Login("admin", "123456")
+        .then((res) => {
+          console.log("Login ok:", res);
+          this.$router.push('/dashboard')
+        })
+        .catch((err) => {
+          console.log(
+            "Login err:",
+            err.response ? err.response.data || "服务器错误" : "网络错误",
+            err
+          );
+
+        });
+    },
+  },
+};
 </script>
+<style lang="sass" scoped>
+.login-dog
+  width: 250px
+  margin-left: 35px
+</style>

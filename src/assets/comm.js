ef9769f0b0217b903d12ebf0e8a5d0730497b1f7
LinskRuis.32G <mgr9525@qq.com>
2021-06-25 18:40:38 +0800

    post

---
+++ src/assets/comm.js
@@ -0,0 +1,5 @@
+// const apiUrl = process.env.NODE_ENV === 'production' ? 'http://gokins.cn:8030' : "http://localhost:8030";
+const apiUrl = 'http://localhost:8030';
+export default {
+    ApiUrl: apiUrl,
+}
--- src/main.js
+++ src/main.js
@@ -5,18 +5,20 @@ import router from './router'
 import CoreuiVue from '@coreui/vue'
 import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
+import api from '@/assets/js/api'
 
-Vue.config.performance = true
-Vue.use(CoreuiVue)
-Vue.prototype.$log = console.log.bind(console)
+Vue.config.performance = true;
+Vue.use(CoreuiVue);
+Vue.prototype.$log = console.log.bind(console);
+Vue.prototype.$post = api.post;
 
 new Vue({
-  el: '#app',
-  router,
-  store,
-  icons,
-  template: '<App/>',
-  components: {
-    App
-  }
+    el: '#app',
+    router,
+    store,
+    icons,
+    template: '<App/>',
+    components: {
+        App
+    }
 })

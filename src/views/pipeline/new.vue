e4c13818e18a178e61a3c990193b1d2ecc47ac45
LinskRuis.32G <mgr9525@qq.com>
2021-07-01 11:54:58 +0800

    bugs

--- src/views/pipeline/new.vue
+++ src/views/pipeline/new.vue
@@ -10,19 +10,18 @@
             <CCardBody>
               <CRow>
                 <CCol>
-                  <CInput label="流水线名称: " placeholder="请输入流水线名称" v-model="formData.name"/>
+                  <CInput label="流水线名称: " placeholder="请输入流水线名称" v-model="formData.name" />
                 </CCol>
               </CRow>
               <CRow>
                 <CCol>
-                  <CInput label="流水线描述: " placeholder="请输入流水线描述"
-                          v-model="formData.displayName"/>
+                  <CInput label="流水线描述: " placeholder="请输入流水线描述" v-model="formData.displayName" />
                 </CCol>
               </CRow>
               <CRow>
                 <CCol>
                   <CInput label="仓库地址: " placeholder="仓库URL地址或者仓库目录(例如:https://xxx.com/test.git 或者 ~/dev/test)"
-                          v-model="formData.url"/>
+                    v-model="formData.url" />
                 </CCol>
               </CRow>
               <CRow>
@@ -32,7 +31,7 @@
                   </codemirror>
                 </CCol>
               </CRow>
-              <hr/>
+              <hr />
               <CRow class="subRow" sm="16">
                 <CCol sm="8">
                   <CButton color="info" @click="subFun()" :disabled="saveBtu">保存</CButton>
@@ -55,14 +54,14 @@
                   </template>
                   <CRow style="margin-top: 10px">
                     <CCol>
-                      <CInput label="账号: " placeholder="clone仓库的账号" v-model="formData.username"/>
+                      <CInput label="账号: " placeholder="clone仓库的账号" v-model="formData.username" />
                       <label class="tips">tips: 如果是公开或者仓库目录模式下,不用填写</label>
                     </CCol>
                   </CRow>
                   <CRow>
                     <CCol>
                       <CInput label="Access Token: " placeholder="clone仓库的用户Access Token"
-                              v-model="formData.accessToken"/>
+                        v-model="formData.accessToken" />
                       <label class="tips">tips: 如果是公开或者仓库目录模式下,不用填写</label>
                     </CCol>
                   </CRow>
@@ -74,7 +73,7 @@
                   <CRow @click="showPlugin(itme)" v-for="(itme, index) in pluginInfos">
                     <CCol>
                       <CWidgetIcon :header="itme.name" :text="itme.content" color="gradient-warning">
-                        <CIcon name="cil-moon" width="24"/>
+                        <CIcon name="cil-moon" width="24" />
                       </CWidgetIcon>
                     </CCol>
                   </CRow>
@@ -90,31 +89,32 @@
         </CCol>
       </CRow>
     </div>
-    <pluginView :shown.sync="pluginShow" :pluginyml.sync="pluginyml"/>
+    <pluginView :shown.sync="pluginShow" :pluginyml.sync="pluginyml" />
   </div>
 </template>
 <script>
-import {codemirror} from 'vue-codemirror'
+import { codemirror } from 'vue-codemirror'
 import "codemirror/addon/lint/lint.css";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/eclipse.css";
 import "codemirror/mode/yaml/yaml";
 import "codemirror/addon/lint/lint";
 import "codemirror/addon/lint/yaml-lint";
-import "codemirror/addon/display/autorefresh"
+import "codemirror/addon/display/autorefresh";
+import jsyaml from "js-yaml";
 
 import pluginView from "@/components/modals/pluginView";
-import {NewPipeline, PipelineInfo, SavePipeline, UtilCatch} from "@/assets/js/apis";
+import { NewPipeline, PipelineInfo, SavePipeline, UtilCatch } from "@/assets/js/apis";
 
-window.jsyaml = require("js-yaml"); // 引入js-yaml为codemirror提高语法检查核心支持
+window.jsyaml = jsyaml;
 export default {
   name: "PipeNew",
-  components: {pluginView, codemirror},
+  components: { pluginView, codemirror },
   props: {
-    editf: false,
+    editf: Boolean,
     pipeId: String
   },
-  data() {
+  data () {
     return {
       formData: {
         name: "",
@@ -127,39 +127,39 @@ export default {
       pluginInfos: [
         {
           name: "git", "content": "git 插件", ymlcontent: "" +
-              "Clone1:\n" +
-              "" +
-              "    git: github\n" +
-              "" +
-              "    repo: https://github.com/koderover/sample\n" +
-              "" +
-              "    revision: master\n" +
-              "" +
-              "    title: git clone\n" +
-              "" +
-              "    type: git-clone\n" +
-              "" +
-              "    working_directory: ${{KR_VOLUME_PATH}}\n" +
-              "" +
-              ""
+            "Clone1:\n" +
+            "" +
+            "    git: github\n" +
+            "" +
+            "    repo: https://github.com/koderover/sample\n" +
+            "" +
+            "    revision: master\n" +
+            "" +
+            "    title: git clone\n" +
+            "" +
+            "    type: git-clone\n" +
+            "" +
+            "    working_directory: ${{KR_VOLUME_PATH}}\n" +
+            "" +
+            ""
         },
         {
           name: "git2", "content": "git2 插件", ymlcontent: "" +
-              "Clone2:\n" +
-              "" +
-              "    git: github\n" +
-              "" +
-              "    repo: https://github.com/koderover/sample\n" +
-              "" +
-              "    revision: master\n" +
-              "" +
-              "    title: git clone\n" +
-              "" +
-              "    type: git-clone\n" +
-              "" +
-              "    working_directory: ${{KR_VOLUME_PATH}}\n" +
-              "" +
-              ""
+            "Clone2:\n" +
+            "" +
+            "    git: github\n" +
+            "" +
+            "    repo: https://github.com/koderover/sample\n" +
+            "" +
+            "    revision: master\n" +
+            "" +
+            "    title: git clone\n" +
+            "" +
+            "    type: git-clone\n" +
+            "" +
+            "    working_directory: ${{KR_VOLUME_PATH}}\n" +
+            "" +
+            ""
         }
       ],
       pluginShow: false,
@@ -174,17 +174,17 @@ export default {
       },
     };
   },
-  mounted() {
+  mounted () {
     if (this.pipeId && this.pipeId != "") {
       this.pipeInfo(this.pipeId)
     }
   },
   methods: {
-    showPlugin(e) {
+    showPlugin (e) {
       this.pluginShow = true
       this.pluginyml = e.ymlcontent
     },
-    subFun() {
+    subFun () {
       this.saveBtu = true
       if (!this.checkForm()) {
         this.saveBtu = false
@@ -196,8 +196,8 @@ export default {
       }
       this.newPie()
     },
-    pipeInfo(id) {
-      PipelineInfo({id: id}).then((res) => {
+    pipeInfo (id) {
+      PipelineInfo({ id: id }).then((res) => {
         this.formData.name = res.data.name
         this.formData.url = res.data.url
         this.formData.username = res.data.username
@@ -206,35 +206,35 @@ export default {
         this.formData.displayName = res.data.displayName
       }).catch((err) => UtilCatch(this, err));
     },
-    newPie() {
+    newPie () {
       NewPipeline(this.formData)
-          .then((res) => {
-            this.saveBtu = false
-            this.$msgOk("保存成功");
-            setTimeout(() => {
-              this.$router.push("/pipeline/info/" + res.data.id)
-            }, 500);
-          })
-          .catch((err) => {
-            this.saveBtu = false
-            UtilCatch(this, err)
-          });
+        .then((res) => {
+          this.saveBtu = false
+          this.$msgOk("保存成功");
+          setTimeout(() => {
+            this.$router.push("/pipeline/info/" + res.data.id)
+          }, 500);
+        })
+        .catch((err) => {
+          this.saveBtu = false
+          UtilCatch(this, err)
+        });
     },
-    savePie() {
+    savePie () {
       let formData = this.formData;
       formData.pipelineId = this.pipeId
       SavePipeline(formData)
-          .then((res) => {
-            this.saveBtu = false
-            this.$msgOk("保存成功");
-            this.pipeInfo(this.pipeId)
-          })
-          .catch((err) => {
-            this.saveBtu = false
-            UtilCatch(this, err)
-          });
+        .then((res) => {
+          this.saveBtu = false
+          this.$msgOk("保存成功");
+          this.pipeInfo(this.pipeId)
+        })
+        .catch((err) => {
+          this.saveBtu = false
+          UtilCatch(this, err)
+        });
     },
-    checkForm() {
+    checkForm () {
       if (!this.formData.name || this.formData.name === "") {
         this.$msgErr("请输入流水线名称");
         return false
@@ -270,8 +270,6 @@ export default {
 
 .tips
   font-size: 5px
-
-
 
 .json-editor
   border: #768192 1px solid

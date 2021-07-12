74e69b02e4c4a473ff804bc297b505e41c2ae12f
LinskRuis.32G <mgr9525@qq.com>
2021-07-12 10:39:43 +0800

    ui bug

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -5,8 +5,8 @@
       <span class="ml-1">&copy; {{new Date().getFullYear()}} creativeLabs.</span>
     </div>
     <div class="mfs-auto">
-      <span class="mr-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <span class="mr-1">Powered by</span>
+      <a href="https://coreui.io/vue" target="_blank">CoreUI for Vue</a>
     </div>
   </CFooter>
 </template>
